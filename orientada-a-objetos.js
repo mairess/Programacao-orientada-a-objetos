aff99c1a6520683b9b402e2d0f450848789ac653
mairess <trabalho.maires@gmail.com>
2023-04-18 07:25:40 -0300

    add orientada-a-objetos.js

---
+++ orientada-a-objetos.js
@@ -0,0 +1,17 @@
+class Poligono {
+    constructor(altura, largura){
+        this.altura = altura
+        this.largura = largura
+    }
+
+    get area(){
+        return this.#calcularArea()
+
+    }
+    #calcularArea(){
+        return this.altura * this.largura
+    }
+}
+
+let poligono = new Poligono(50,60)
+console.log(poligono.area)
